b82c90ef223f714d52e0ce6e5676ea292d1f6f2b
Aditya Soran <adityasoran@gmail.com>
2025-11-18 20:36:10 +0530

    Initial commit: AyurSutra  frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  base: "./", 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
