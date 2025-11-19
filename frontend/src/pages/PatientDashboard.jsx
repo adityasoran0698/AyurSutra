de14f1d8262d89c5c6e51ea50da2f24be1ebd5bf
Aditya Soran <adityasoran@gmail.com>
2025-11-19 18:00:16 +0530

    Initial commit: AyurSutra  frontend

--- frontend/src/pages/PatientDashboard.jsx
+++ frontend/src/pages/PatientDashboard.jsx
@@ -331,7 +331,7 @@ export default function PatientDashboard() {
                         expandedBookingId === b._id ? null : b._id
                       )
                     }
-                    className=" px-3 py-1  bg-slate-200 rounded text-sm w-full sm:w-auto"
+                    className=" px-3 py-1  bg-slate-200 rounded text-sm w-full sm:w-auto whitespace-nowrap"
                   >
                     {expandedBookingId === b._id ? "Collapse" : "See Status"}
                   </button>
